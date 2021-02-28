09fedc112be72c7b053aebf80711f999461e9ff0
Deguta <tech.ym1110@gmail.com>
2021-02-28 21:05:14 +0900

    MASTER UPDATE
    検索フォームを作成

--- resources/views/bulletin-boards/index.blade.php
+++ resources/views/bulletin-boards/index.blade.php
@@ -15,6 +15,7 @@
 <div class="mt-4 mb-4">
     <a href="{{ route('bulletin-board.create') }}"  class="create-btn m-auto">投稿の新規作成はこちらです</a>
 </div>
+
 @if (session('flash_message'))
     <div class="alert alert-success mt-4 mb-4">
         {{ session('flash_message') }}
@@ -24,68 +25,51 @@
 <div class="table-responsive">
     <table class="table table-hover">
         <thead>
-        <tr>
-            <th>ID</th>
-            <th>カテゴリ</th>
-            <th>作成日時</th>
-            <th>名前</th>
-            <th>件名</th>
-            <th>メッセージ</th>
-            <th>処理</th>
-        </tr>
-        </thead>
-            <div class="mt-4 mb-4">
-                <form class="form-inline" method="GET" action="{{ route('bulletin-board.index') }}">
-                <div class="input-group">
-                    <input type="text" class="form-control" placeholder="テキスト入力欄">
-                    <span class="input-group-btn">
-                        <button type="button" class="btn btn-default">ボタン</button>
-                    </span>
-                </div>
-                    <div class="form-group">
-                        <input type="text" name="searchword" value="{{$searchword}}" class="form-control" placeholder="名前を入力してください">
-                    </div>
-                    <input type="submit" value="検索" class="btn btn-info ml-2">
-                    <span class="input-group-btn">
-                        <button type="button" class="btn btn-default">ボタン</button>
-                    </span>
-                </form>
-            </div>
-        <tbody id="tbl">
-        @foreach ($posts as $post)
             <tr>
-                <td>{{ $post->id }}</td>
-                <td>{{ $post->category }}</td>
-                <td>{{ $post->created_at->format('Y.m.d') }}</td>
-                <td>{{ $post->name }}</td>
-                <td>{{ $post->subject }}</td>
-                <td>{!! nl2br(e(Str::limit($post->message, 100))) !!}
-                @if ($post->comments->count() >= 1)
-                    <p><span class="badge badge-primary">
-                    現在のコメント数は{{ $post->comments->count() }}件
-                    </span></p> //countでコメントが何件あるか表示できる
-                @endif
-                </td>
-                <td class="text-nowrap">
-                    <p><a href="{{ route('bulletin-board.show', $post) }}" class="btn btn-primary btn-sm">詳細</a></p>
-                    <p><a href="{{ action('PostsController@edit', $post) }}" class="btn btn-info btn-sm">編集</a></p>
-                    <p>
-                        <form method="POST" action="{{ route('bulletin-board.destroy', $post->id) }}">
-                            @csrf
-                            @method('DELETE')
-                            <button class="btn btn-danger btn-sm">削除</button>
-                        </form>
-                        
-                    </p>
-                </td>
+                <th>ID</th>
+                <th>カテゴリ</th>
+                <th>作成日時</th>
+                <th>名前</th>
+                <th>件名</th>
+                <th>メッセージ</th>
+                <th>処理</th>
             </tr>
-        @endforeach
+        </thead>
+
+        <tbody id="tbl">
+            @foreach ($posts as $post)
+                <tr>
+                    <td>{{ $post->id }}</td>
+                    <td>{{ $post->category }}</td>
+                    <td>{{ $post->created_at->format('Y.m.d') }}</td>
+                    <td>{{ $post->name }}</td>
+                    <td>{{ $post->subject }}</td>
+                    <td>{!! nl2br(e(Str::limit($post->message, 100))) !!}
+                    @if ($post->comments->count() >= 1)
+                        <p><span class="badge badge-primary">
+                        現在のコメント数は{{ $post->comments->count() }}件
+                        </span></p> //countでコメントが何件あるか表示できる
+                    @endif
+                    </td>
+                    <td class="text-nowrap">
+                        <p><a href="{{ route('bulletin-board.show', $post) }}" class="btn btn-primary btn-sm">詳細</a></p>
+                        <p><a href="{{ action('PostsController@edit', $post) }}" class="btn btn-info btn-sm">編集</a></p>
+                        <p>
+                            <form method="POST" action="{{ route('bulletin-board.destroy', $post->id) }}">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-danger btn-sm">削除</button>
+                            </form>
+                        </p>
+                    </td>
+                </tr>
+            @endforeach
         </tbody>
     </table>
     <div class="d-flex justify-content-center mb-5">
-    {{ $posts->appends(['searchword' => $searchword])->links() }}
-</div>
-</div>
+        {{ $posts->appends(['searchword' => $searchword])->links() }}
+    </div>
+</div>  
 @endsection
  
 @include('layout.bulletin-board-footer')
--- resources/views/layout/bulletin-board-header.blade.php
+++ resources/views/layout/bulletin-board-header.blade.php
@@ -1,7 +1,12 @@
 @section('header')
-<header class="navbar navbar-dark bg-primary ">
-    <div class="container">
-        <a class="navbar-brand font-weight-bold" href="{{ route('bulletin-board.index') }}">Post-Site</a>
-    </div>
-</header>
+    <header class="navbar navbar-dark bg-primary ">
+        <div class="container">
+            <a class="navbar-brand font-weight-bold" href="{{ route('bulletin-board.index') }}">Post-Site</a>
+
+            <form class="form-inline" method="GET" action="{{ route('bulletin-board.index') }}">
+                <input type="text" name="searchword" class="form-control mr-sm-2" value="{{$searchword}}" aria-label="searchword" placeholder="キーワードを入力">
+                <button type="submit" class="btn btn-info">検索</button>
+            </form>
+        </div>
+    </header>
 @endsection
