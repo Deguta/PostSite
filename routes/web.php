045512621d93b0952c0283166728eb634fa4c7af
Deguta <tech.ym1110@gmail.com>
2021-04-11 23:15:44 +0900

    MASTER UPDATE
    メールフォームのrouteとコントローラーを作成

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,7 @@ Route::resource('bulletin-board', 'PostsController', ['only' => ['index','create
 
 Route::resource('comment', 'CommentsController',['only' => ['store']]);
 
-Log::channel('single')->error('Something happends');
+// お問い合わせの送信メールのルーティング
+Route::get('/contact/index', 'ContactController@index')->name('contact.index');
+Route::post('/contact/confirm', 'ContactController@confirm')->name('contact.confirm');
+Route::post('/contact/complete', 'ContactController@send')->name('contact.send');
