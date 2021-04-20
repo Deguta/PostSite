f60700b7a2c7ff61050c34351b3e11c5cc4d0557
Deguta <tech.ym1110@gmail.com>
2021-04-20 23:46:00 +0900

    MASTER UPDATE
    BGのカラーを変更

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Mail\ContactSendmail;
 
-
 class ContactController extends Controller
 {
     public function index(){
--- resources/views/contacts/complete.blade.php
+++ resources/views/contacts/complete.blade.php
@@ -13,7 +13,7 @@
 @section('content')
 <div class="container">
   <div class="panel-default  w-75  mx-auto card mt-5">
-    <div class="panel-heading  h2 p-3 text-center bg-primary text-white">お問い合わせフォーム 確認画面</div>
+    <div class="panel-heading  h2 p-3 text-center bg-dark text-white">お問い合わせフォーム 確認画面</div>
     <div class="panel-body">
       <div class="form-group">
         <div class="h1 text-center">{{ __('送信が完了しました') }}</div>
